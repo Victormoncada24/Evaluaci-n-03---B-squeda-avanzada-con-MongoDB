299f0d43b29f6adc1ef86e38daaf562c64ea49c5
Manix <victor.moncada001@gmail.com>
2026-07-02 21:29:03 -0400

    Clonacion de repositorio, en el cual se trabajo en clase

---
+++ EVA 3/frontend/scripts/inicio.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    obtenerUsuarios();
+};
+
+async function obtenerUsuarios() {
+    try {
+        const respuesta = await fetch('http://localhost:3000/usuarios');
+        const usuarios = await respuesta.json();
+
+        console.log(usuarios)
+
+        new DataTable('#tablaUsuarios',{
+            data: usuarios,
+            columns:[
+                {data: 'nombre'},
+                {data: 'email'},
+                {data: 'rut'},
+                {data: 'telefono'},
+                {data: 'nacimiento'},
+                {data: 'genero'},
+                {data: 'nacionalidad'},
+            ]
+    }); 
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+};
